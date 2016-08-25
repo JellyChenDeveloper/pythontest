c7cae2bd5d901f24d03173996799fa5d6f7eff94
JellyChen_Office <451453325@qq.com>
2016-08-25 18:15:22 +0800

    完成抓取百度贴吧图片脚本

---
+++ crawler/getjpg.py
@@ -0,0 +1,26 @@
+# coding=utf-8
+import urllib
+import re
+
+
+def getHtml(url):
+    page = urllib.urlopen(url)
+    html = page.read()
+    return html
+
+
+# 根据网页内容解析图片链接并下载图片至本地
+def getImg(html):
+    rex = r'src="(http://imgsrc\.baidu\.com/forum/.+?\.jpg)"'
+    imglist = re.findall(rex, html)
+    x = 0
+    for imgurl in imglist:
+        urllib.urlretrieve(imgurl, '%s.jpg' % x)  # 下载图片至本地并规范化文件名
+        x += 1
+
+
+html = getHtml("http://tieba.baidu.com/p/4745904310")
+
+print getImg(html)
+
+# http://imgsrc.baidu.com/forum/w%3D580/sign=e6e6f428ac0f4bfb8cd09e5c334e788f/be248d44ad345982d6537e2504f431adc9ef84a1.jpg
